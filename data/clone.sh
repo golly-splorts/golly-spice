bfcda8edc276197785339048e08b748d0631ac6a
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2022-07-30 21:36:32 -0700

    add clone.sh

---
+++ data/clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone git@github.com:golly-splorts/gollyx-hellmouth-data
+
+git clone git@github.com:golly-splorts/gollyx-pseudo-data
+git clone git@github.com:golly-splorts/gollyx-toroidal-data
+
+git clone git@github.com:golly-splorts/gollyx-dragon-data
+git clone git@github.com:golly-splorts/gollyx-rainbow-data
+
+git clone git@github.com:golly-splorts/gollyx-klein-data
+git clone git@github.com:golly-splorts/gollyx-star-data
