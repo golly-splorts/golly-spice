39f7a08e9688a1b374cf2ca1bd3816a44e0c1c3b
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2022-08-06 12:24:41 -0700

    update data clone script

--- data/clone.sh
+++ data/clone.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git clone git@github.com:golly-splorts/golly-data gollyx-hellmouth-data
+git clone git@ch4zm.github.com:golly-splorts/golly-data gollyx-hellmouth-data
 
-git clone git@github.com:golly-splorts/gollyx-pseudo-data
-git clone git@github.com:golly-splorts/gollyx-toroidal-data
+git clone git@ch4zm.github.com:golly-splorts/gollyx-pseudo-data
+git clone git@ch4zm.github.com:golly-splorts/gollyx-toroidal-data
 
-git clone git@github.com:golly-splorts/gollyx-dragon-data
-git clone git@github.com:golly-splorts/gollyx-rainbow-data
+git clone git@ch4zm.github.com:golly-splorts/gollyx-dragon-data
+git clone git@ch4zm.github.com:golly-splorts/gollyx-rainbow-data
 
-git clone git@github.com:golly-splorts/gollyx-klein-data
-git clone git@github.com:golly-splorts/gollyx-star-data
+git clone git@ch4zm.github.com:golly-splorts/gollyx-klein-data
+git clone git@ch4zm.github.com:golly-splorts/gollyx-star-data
